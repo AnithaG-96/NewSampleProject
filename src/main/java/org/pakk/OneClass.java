a3b08c21414564c703606c00bcc82ed3e1fd6724
AnithaG-96 <anithagk96@gmail.com>
2022-03-16 18:23:19 +0530

    code1

---
+++ src/main/java/org/pakk/OneClass.java
@@ -0,0 +1,32 @@
+package org.pakk;
+
+public class OneClass {
+private void method1() {
+System.out.println("MethodA1");
+}
+private void method2() {
+System.out.println("MethodA2");
+}
+private void method3() {
+System.out.println("MethodA3");
+}
+public static void main(String[] args) {
+	OneClass o=new OneClass();
+	o.method1();
+	o.method2();
+	o.method3();
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
